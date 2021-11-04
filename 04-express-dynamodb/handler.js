9d61d5c8d3582ed635b4e55665f1b876747b8d12
Juan Carlos Valdés <idevkingos@gmail.com>
2021-11-04 10:15:14 -0300

    feat: configuro express

---
+++ 04-express-dynamodb/handler.js
@@ -0,0 +1,10 @@
+"use strict";
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+module.exports.generic = serverless(app);
